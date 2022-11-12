1192f533518a587a444db0356b24f1932b9e1d68
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2022-11-12 12:22:14 +0600

    Create Problem C. Bear and Reverse Radewoosh.cpp

---
+++ RU CSE Practice Contest Nov 11, 2022/Problem C. Bear and Reverse Radewoosh.cpp
@@ -0,0 +1,76 @@
+
+ 
+ /* @sujan prodhan
+ 
+ 
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠴⠒⠃⠉⠁⠒⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⣀⣠⣴⣆⡤⠦⢄⠀⠀⠀⠀⠀⣾⠤⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣀⣀⣸⠇⠀⠀⠀⡀⢸⣆⠀⠀⠉⠑⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢽⡞⠓⣿⢷⣄⠀⠀⢀⡼⢀⠏⢸⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⡶⠶⠶⠤⠤⣤⣤⣤⣤⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠸⣤⡴⠁⠀⢉⣇⣠⠟⣡⡎⠀⢸⠀⠀⢀⠀⠀⠀⠀⠀⠈⢧⠀⠀⠀⠀⠀⠀
+⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠹⣆⠀⠀⠀⠀⠀⢀⡇⢱⣶⣾⢛⣉⣻⣉⢹⠉⠉⠋⠀⠀⢘⡆⠀⠀⠀⢀⣤⠀⢳⠀⠀⠀⠀⠀
+⠀⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠀⢀⡇⢸⡿⠿⠿⠿⠿⣿⣿⡤⠤⠤⢤⠀⠀⢷⣠⠀⣠⣿⠏⠀⠀⢧⠀⠀⠀⠀
+⠘⠓⠒⠒⠷⡀⠀sujan⠀⠀⠀⠀⠹⣦⠤⠶⣶⠾⠗⠺⣄⣀⣀⣐⣮⣿⡶⣒⡉⠁⠘⣷⣀⣘⣿⣞⣡⢔⣂⣀⣀⡘⡄⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⣄⠀⠀⠀prodhan⠹⣦⠤⢼⣇⣀⣀⠀⠈⡇⠀⠀⡟⢯⠤⠤⠀⠀⣿⣏⠙⢯⠉⠛⠍⠉⠉⠉⠙⢧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣾⣿⣿⣿⠟⠻⠶⢶⣇⣬⠿⠶⢶⣾⣿⠏⠀⣸⠀⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⠢⠤⣀⣀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⡷⠶⠾⠛⠋⠀⠀⠀⠈⠙⠒⠓⠒⠂⠠⠤⠤⠤⠤⢼⣁⣀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠒⠲⠦⢤⣤⣽⣏⡙⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸
+ *
+ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+
+    int n,c;
+    cin>>n>>c;
+    int score[n],minutes[n], totalTime =0,totalpoint=0,totalTimeR=0,totalpointR =0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>score[i];
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>minutes[i];
+    }
+    //LIMAK
+    for(int i=0;i<n;i++)
+    {
+        totalTime =totalTime + minutes[i];
+      // totalpoint =  totalpoint+score[i] - totalTime *c;
+      totalpoint = totalpoint + max(0,score[i] - c* totalTime);
+    }
+
+
+    cout<<totalpoint<<endl;
+    //RADEWOOSH
+     for(int i=n-1;i>=0;i--)
+    {
+        totalTimeR =totalTimeR + minutes[i];
+      // totalpointR =  totalpointR+score[i] - totalTimeR *c;
+      totalpointR =  totalpointR+max(0,score[i] - totalTimeR *c);
+    }
+
+    cout<<totalpointR<<endl;
+
+
+    if(totalpoint > totalpointR)
+    {
+        cout<<"Limak"<<endl;
+    }
+    else if(totalpoint < totalpointR)
+      {
+        cout<<"Radewoosh"<<endl;
+    }
+    else
+        {
+        cout<<"Tie"<<endl;
+    }
+}
